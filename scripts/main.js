a14f1356bf0a79ec92500679ba5bf3ab51f90096
Muthabuli <36258903+Muthabuli@users.noreply.github.com>
2018-02-08 16:07:52 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/zwo2.jpg') {
+      myImage.setAttribute ('src','images/zwo.jpg');
+    }  else {
+      myImage.setAttribute ('src','images/zwo2.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Java script is cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Java script is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
